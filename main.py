9e5a0f268b7e22ceae6ecb1bd5b68a742f6d720b
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-04-19 12:09:47 +0200

    Implement data upload

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import os
 import pathlib
 import requests
 import logging
+import pandas as pd
 
-logging.basicConfig(filename='upload.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename="upload.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
-OEP_USER = os.environ.get('OEP_USER')
-OEP_TOKEN = os.environ.get('OEP_TOKEN')
+OEP_USER = os.environ.get("OEP_USER")
+OEP_TOKEN = os.environ.get("OEP_TOKEN")
 
-OEP_API = "https://openenergyplatform.org/api/v0/schema/model_draft/tables"
+OEP_API = "https://openenergyplatform.org/api/v0"
 OEDATAMODEL_API_URL = "https://modex.rl-institut.de"
 
 
@@ -22,7 +27,7 @@ def load_credentials():
 
 
 def table_exists(table_name: str) -> bool:
-    response = requests.get(f"{OEP_API}/{table_name}")
+    response = requests.get(f"{OEP_API}/schema/model_draft/tables/{table_name}")
     if response.status_code == 200:
         return True
     return False
@@ -30,16 +35,20 @@ def table_exists(table_name: str) -> bool:
 
 def create_table(table_name: str, metadata_filename: pathlib.Path):
     files = {"metadata_file": open(metadata_filename, "rb")}
-    response = requests.post(f"{OEDATAMODEL_API_URL}/create_table/", files=files, data={"user": OEP_USER, "token": OEP_TOKEN})
+    response = requests.post(
+        f"{OEDATAMODEL_API_URL}/create_table/",
+        files=files,
+        data={"user": OEP_USER, "token": OEP_TOKEN},
+    )
     if response.status_code == 200:
         logging.info(f"Table {table_name} created successfully.")
     else:
-        logging.error(f"Table {table_name} could not be created. Reason: {response.text}")
+        logging.error(
+            f"Table {table_name} could not be created. Reason: {response.text}"
+        )
 
 
-def create_tables_from_folder(folder: pathlib.Path | str):
-    if isinstance(folder, str):
-        folder = pathlib.Path(folder)
+def create_tables_from_folder(folder: pathlib.Path):
     for metadata_filename in folder.iterdir():
         if not metadata_filename.suffix == ".json":
             continue
@@ -55,9 +64,57 @@ def create_tables_from_folder(folder: pathlib.Path | str):
         create_table(table_name, metadata_filename)
 
 
+def version_exists(table_name: str, version: str) -> bool:
+    data = {
+        "query": {
+            "fields": ["version"],
+            "from": {"type": "table", "table": table_name, "schema": "model_draft"},
+        }
+    }
+    response = requests.post(f"{OEP_API}/advanced/search", json=data)
+    rows = response.json()["data"]
+    versions = {row[0] for row in rows}
+    return version in versions
+
+
+def upload_data(table_name: str, data_file: pathlib.Path):
+    files = {"csv_file": open(data_file, "rb")}
+    response = requests.post(
+        f"{OEDATAMODEL_API_URL}/upload/",
+        files=files,
+        data={"token": OEP_TOKEN, "schema": "model_draft", "table": table_name, "adapt_pks": "on"},
+    )
+    if response.status_code == 200:
+        logging.info(f"Dataset uploaded successfully for table {table_name}.")
+    else:
+        logging.error(f"Dataset upload failed for table {table_name}. Reason: {response.text}")
+
+
+def upload_files_form_folder(folder: pathlib.Path):
+    for data_filename in folder.iterdir():
+        if not data_filename.suffix == ".csv":
+            continue
+
+        with open(data_filename, "r") as f:
+            data = pd.read_csv(f, delimiter=";")
+
+        table_name = data_filename.name.split(".")[0]
+        version = data.iloc[0]["version"]
+
+        if version_exists(table_name, version):
+            logging.info(
+                f"Version {version} already exists in table {table_name}. Skipping."
+            )
+            continue
+
+        upload_data(table_name, data_filename)
+
+
 if __name__ == "__main__":
     load_credentials()
     upload_folder = input("Input folder (default: data/): ")
     if upload_folder == "":
         upload_folder = "data"
+    upload_folder = pathlib.Path(upload_folder)
     create_tables_from_folder(upload_folder)
+    upload_files_form_folder(upload_folder)
